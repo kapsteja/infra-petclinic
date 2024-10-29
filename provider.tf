d882dc13086791cb2325141417aa433964cc9b5f
chenchuteja jeelaga <chenchuteja_jeelaga@epam.com>
2024-10-29 22:59:17 +0530

    modified

--- provider.tf
+++ provider.tf
@@ -1,15 +1,7 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # AWS PROVIDER FOR TF CLOUD
 # ---------------------------------------------------------------------------------------------------------------------
-terraform {
-  backend "s3" {
-    bucket         = "tdsterraformstatefile"  
-    key            = "tdsterraformstatefile/statefile/terraform.tfstate"    
-    region         = "us-east-1"                    
-    encrypt        = true                           
-    dynamodb_table = "terraform-lock-table"         
-  }
-}
+
 
 provider "aws" {
   region  = "us-east-1"
